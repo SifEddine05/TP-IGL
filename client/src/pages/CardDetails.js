6d01d38ec40aa88434926c26da2b9902890d3a75
SifEddine05 <sifousamir72@gmail.com>
2023-04-27 00:19:27 +0100

    fix ui of CardDetails page

--- client/src/pages/CardDetails.js
+++ client/src/pages/CardDetails.js
@@ -73,9 +73,9 @@ const CardDetails = () => {
                <div className="lg:text-lg md:text-md sm:text-sm text-xs">{Card.userName}</div>
 
 
-               <div className=" pt-[2%]  w-full flex justify-between ">
+               <div className=" pt-[2%]  w-full flex sm:flex-row flex-col items-center justify-between ">
 
-                    <div className="w-[45%] flex flex-col justify-center items-center ">
+                    <div className="sm:w-[45%] w-[85%] flex flex-col justify-center items-center ">
                          <div className='w-[80%]'>
                          <a target="_blank"  rel="noreferrer" href={Card.images[0]} className='w-full '><img src={Card.images[0]} alt='pp' className="w-[100%] " /></a> 
                          </div>
@@ -88,12 +88,12 @@ const CardDetails = () => {
                          </div>
                          
                     </div>
-                    <div className="w-[45%] ">
-                         <div className="flex justify-between items-center   ">
-                              <div className="text-[#FF5D02] font-bold w-full lg:text-xl md:text-lg sm:text-md text-xs">{Card.price} DA</div>
-                              <div className="w-full ml-[20%]  "><Link to="/sendMsg" className="text-black lg:text-xl md:text-lg sm:text-md text-xs bg-[#FF5D02] hover:bg-gray-600  hover:text-white rounded-full pt-[5%] pb-[5%] pl-[10%] pr-[10%]">Buy this appartment</Link></div>
+                    <div className="sm:w-[45%] w-[85%]">
+                         <div className="flex justify-between items-center mt-4  gap-4 w-full">
+                              <div className="text-[#FF5D02] font-bold w-fit lg:text-xl md:text-lg sm:text-md text-xs">{Card.price} DA</div>
+                              <div className="w-fit self-end"><Link to="/sendMsg" className=" text-black lg:text-xl md:text-lg sm:text-sm text-xs bg-[#FF5D02] hover:bg-gray-600  hover:text-white rounded-full pt-[5%] pb-[5%] px-4">Buy this appartment</Link></div>
                          </div>
-                         <div className="mt-[5%] lg:text-sm md:text-xs sm:text-[12px] text-[10px] ">About this property</div>
+                         <div className="mt-[5%] lg:text-xl md:text-lg sm:text-md text-xs font-bold">About this property</div>
                          <div className=" flex justify-between pl-[5%] pr-[5%] pt-[2%] pb-[2%] mt-[5%]  text-black w-full">
                               <div className='flex flex-col justify-center items-center'>
                                    <div className="lg:text-xl md:text-lg sm:text-md text-xs">Area</div>
@@ -109,19 +109,15 @@ const CardDetails = () => {
                                    <div className="lg:text-xl md:text-lg sm:text-md text-xs font-bold">{Card.nbrRooms}</div>
                               </div>
                          </div>
-                         <div className=" mt-[5%] lg:text-xl md:text-lg sm:text-md text-xs">Desctiption</div>
+                         <div className=" mt-[5%] lg:text-xl md:text-lg sm:text-md text-xs font-bold">Desctiption</div>
                          <div className="mt-[5%] lg:text-xl md:text-lg sm:text-md text-xs">{Card.description}</div>
-                         <div className="mt-[5%] lg:text-xl md:text-lg sm:text-md text-xs">Location</div>
+                         <div className="mt-[5%] lg:text-xl md:text-lg sm:text-md text-xs font-bold">Location</div>
                          <div className="mt-[3%] lg:text-xl md:text-lg sm:text-md text-xs">{Card.street} , {Card.city} </div>
                          <div className="w-full flex mt-[5%] justify-around mx-auto">
-                              <a  href="#my-modal-2" className="w-full ml-[5%] "><button className="flex  md:px-6 px-3  items-center rounded-full md:p-3 p-2  hover:bg-black hover:bg-opacity-75 bg-black  " >
+                              <a  href="#my-modal-2" className="w- ml-[5%] "><button className="flex  md:px-6 px-3  items-center rounded-full md:p-3 p-2  hover:bg-[#FF5D02] bg-black  " >
                                    <div className="flex"><img src={aa} alt='aa'/></div>
                                    <div className="w-full pl-[2%] text-white lg:text-[16px] md:text-[14px] sm:text-[12px] text-[10px]">Call owner</div>
                                    </button></a>
-                              <div className="w-[50%]  "><Link to='/sendMsg' className=" flex items-center  md:p-3 p-2  rounded-full border-2 border-black hover:bg-black hover:bg-opacity-10">
-                                   <div><img src={tt} alt='tt'/></div>
-                                   <div className=" w-full pl-[2%] lg:text-[16px] md:text-[14px] sm:text-[12px] text-[10px]">Message owner</div>
-                                   </Link></div>
                          </div>
                     </div>
                </div>
@@ -129,11 +125,11 @@ const CardDetails = () => {
            </div>
 
           <div className="modal" id="my-modal-2">
-               <div className="modal-box">
+               <div className="modal-box bg-white">
                <h3 className="font-bold text-lg">Would you Call the owner ! </h3>
                <p className="py-4">This is his phone number : 0558456920 </p>
                <div className="modal-action">
-               <a href="#" className="btn"> OK! </a>    
+               <a href="#" className="btn bg-black text-white"> OK! </a>    
           </div>
   </div>
 </div> 
