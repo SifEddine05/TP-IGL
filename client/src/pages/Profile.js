0e7d2f029390e181ceac2571d4803f7077936586
SifEddine05 <sifousamir72@gmail.com>
2023-04-27 00:25:19 +0100

    fix ui of Profile page

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -145,15 +145,15 @@ const Profile = () => {
   }
   
     return ( 
-       <div>
+       <div className='bg-white h-full'>
             <Nav num={false} /> 
             <div className="m-[3%] ">
-            <div className="rounded-full bg-white  flex justify-between  p-2 ">
+            <div className="rounded-full bg-white  flex justify-between  p-2  ">
                 <div className="md:w-[20%] lg:w-[15%] sm:w-[20%] w-[25%] flex justify-between items-center">
                     <div className="w-[40%] "><img src={ii} alt='ii' className="rounded-full "/></div>
                     <div className=" lg:text-[20px] md:text-[18px] sm:text-[16px] text-[14px]">Hi , {auth.user.firstName}</div>
                 </div>
-                <div className="w-[50%] flex justify-around">
+                <div className=" flex justify-around gap-4 sm:gap-12">
                     <div className="flex flex-col justify-center items-center">
                         <div className="lg:text-[16px] md:text-[14px] sm:text-[12px] text-[10px]">My Listings</div>
                         <div className=" lg:text-[18px] md:text-[16px] sm:text-[14px] text-[12px] font-bold">{MyListing.length}</div>
@@ -172,7 +172,7 @@ const Profile = () => {
                 <div className='md:w-[10%] w-[15%]'><img src={mm} alt='mm' className='w-full'/></div>
                 <div className="lg:text-[16px] md:text-[14px] sm:text-[12px] text-[10px] ml-[1%] mt-[0.25%]">My listings</div>
             </div>
-            <div className='lg:mt-4 md:mt-3 sm:mt-2 mt-1 mx-auto flex justify-around flex-wrap items-center w-[100%]'>
+            <div className='lg:mt-4 md:mt-3 sm:mt-2 mt-1 mx-auto flex justify-around flex-wrap items-center w-[100%] gap-2'>
             {err && <div  className="w-[100%] mx-auto lg:pt-2 pt-1" >
                     <p className="text-center font-bold lg:text-[18px] md:text-[16px] sm:text-[13px] text-[10px] text-red-600 ">{message}</p>
                 </div> }
@@ -180,7 +180,7 @@ const Profile = () => {
                 {MyListing.map((e)=>{
                     return( 
                    // <div className="flex flex-col justify-center items-center w-full">
-                   <div className="flex flex-col justify-center items-center w-[20%] lg:w-[18%]" >
+                   <div className="flex flex-col justify-center items-center w-[30%] lg:w-[18%] " >
                        <Card annonce={e} className='w-full'/>
                         <button  onClick={()=>{deleteAnonce(e.id)}} className=" group flex flex-col justify-center items-center w-[50%] hover:bg-[#000] hover:bg-opacity-20  rounded-full p-2 border-2 " > 
                             <h4 className='text-red-600 hidden group-hover:block'> Delete Annonce </h4>
